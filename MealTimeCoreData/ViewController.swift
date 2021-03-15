8ff6904c2111e9d717d0d02619953fcef50b0ea4
Moiseev Andrei <andreimoiseew2009@yandex.ru>
2021-03-15 23:58:26 +0300

    Initial Commit

---
+++ MealTimeCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealTimeCoreData
+//
+//  Created by Андрей Моисеев on 15.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
